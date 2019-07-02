6d2324c42fdbe1fb95343a850e6f588cd3fa8ddc
James <hayward.jp@gmail.com>
2019-07-02 15:23:26 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+function mapToNegativize(array) {
+   return array.map(element => element = -element)
+};
+
+function mapToNoChange(array) {
+   return array.map(element => element);
+};
+
+function mapToDouble(array) {
+   return array.map(element => element*2);
+};
+
+function mapToSquare(array) {
+   return array.map(element => element**2);
+};
+
+function reduceToTotal(array, start = 0) {
+   for(let i = 0; i < array.length; i++) {
+      start += array[i];
+   }
+   return start;
+};
+
+function reduceToAllTrue(array) {
+   for(let i = 0; i < array.length; i++) {
+      if (array[i] === false) {
+         return false;
+      }
+   } 
+   return true;
+};
+
+function reduceToAnyTrue(array) {
+   for(let i = 0; i < array.length; i++) {
+      if (array[i] === true) {
+         return true;
+      }
+   } 
+   return false;
+};
