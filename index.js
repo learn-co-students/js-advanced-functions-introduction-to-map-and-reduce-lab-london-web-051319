7c09dff69e9fcc2020517e2bc03aaa8f190634a2
flexkat <catherinemaritz1@gmail.com>
2019-07-02 16:34:27 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(element => {
+    let newElement = element * -1;
+    newArray.push(newElement);
+    
+  });
+  return newArray
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = sourceArray;
+  return newArray;
+}
+
+function mapToDouble(array) {
+  let doubledArray = [];
+  array.forEach(element => {
+    doubledArray.push(element * 2);
+  })
+  return doubledArray;
+}
+
+function mapToSquare(array) {
+  let squaredArray = [];
+  array.forEach(element => {
+    squaredArray.push(element * element);
+  })
+  return squaredArray;
+}
+
+function reduceToTotal(array, startingPoint = 0) {
+  let total = startingPoint;
+  array.forEach(element => {
+    total += element
+  })
+  return total;
+}
+
+function reduceToAllTrue(array) {
+  let truthy = true;
+  for(let element of array){
+    if(!!element == true) {
+      truthy = true;
+    }
+    else {
+      truthy = false
+      break
+    }
+   }
+   return truthy;
+}
+
+function reduceToAnyTrue(array) {
+  if (array.includes(true)){
+    return true
+  }
+  else {
+    return false;
+  }
+}
